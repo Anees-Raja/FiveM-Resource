6fa331863692ad5c29449554a417839f993ec9dc
Anees Raja <aneesraja1995@gmail.com>
2019-04-07 20:53:10 -0400

    myid command added - not sure if this is async

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,7 @@
+function msg(msgBody: string) {
+  TriggerEvent("chatMessage", "[Server]", "red", msgBody)
+}
+
 function alert(msg: string) {
   SetTextComponentFormat("STRING")
   AddTextComponentString(msg)
@@ -8,42 +12,8 @@ function giveWeapon(hash: any) {
   GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(hash), 999, false, false)
 }
 
-// (function () {
-//   let h_key = 74
-//   let flag = true
-//   // let x_key = 73
-
-//   while(flag) {
-//     Wait(1)
-//     if(IsControlReleased(1, h_key)) {
-//       console.log("The key was pressed: ", h_key)
-//       giveWeapon("WEAPON_SMG")
-//       giveWeapon("WEAPON_PUMPSHOTGUN")
-//       alert("~b~Given Weapons with ~INPUT_VEH_HEADLIGHT~")
-//       flag = false
-//     }
-//   }
-// })();
-
-// setImmediate(() => {
-//   let h_key = 74
-//   let flag = true
-//   // let x_key = 73
-
-//   while(flag) {
-//     Wait(1)
-//     if(IsControlReleased(1, h_key)) {
-//       console.log("The key was pressed: ", h_key)
-//       giveWeapon("WEAPON_SMG")
-//       giveWeapon("WEAPON_PUMPSHOTGUN")
-//       alert("~b~Given Weapons with ~INPUT_VEH_HEADLIGHT~")
-//       flag = false
-//     }
-//   }
-// })
-
 RegisterCommand(
-  "give", 
+  "guns", 
   function() {
     giveWeapon("WEAPON_SMG")
     giveWeapon("WEAPON_PUMPSHOTGUN")
@@ -51,3 +21,26 @@ RegisterCommand(
   }, 
   false
 );
+
+RegisterCommand(
+  "myid", 
+  function() {
+    let playerId = GetPlayerServerId(-1)
+    msg(`Your ID is: ${playerId}`)
+  }, 
+  false
+);
+
+
+// Listens to see if "H" was pressed in the last 15ms
+setInterval(() => {
+  let h_key = 74
+  let x_key = 73
+  
+  if(IsControlJustPressed(1, h_key)) {
+    console.log("The key was pressed: ", h_key)
+    giveWeapon("WEAPON_SMG")
+    giveWeapon("WEAPON_PUMPSHOTGUN")
+    alert("~b~Given Weapons with ~INPUT_VEH_HEADLIGHT~")
+  }
+}, 15)
